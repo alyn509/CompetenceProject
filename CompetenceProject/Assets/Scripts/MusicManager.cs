4370985ccc76dd0a08481df50c1acee663a051e7
Mia <Mia@sogaard.us>
2016-10-21 12:18:15 +0200

    Slowly adding sound manager.

--- CompetenceProject/Assets/Scripts/MusicManager.cs
+++ CompetenceProject/Assets/Scripts/MusicManager.cs
@@ -3,13 +3,110 @@ using System.Collections;
 
 public class MusicManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public AudioClip musicClip1;                   //Drag a reference to the audio source which will play the sound effects.
+    public AudioClip musicClip2;                 //Drag a reference to the audio source which will play the music.
+    public static MusicManager instance = null;     //Allows other scripts to call functions from SoundManager.             
+    public float lowPitchRange = .95f;              //The lowest a sound effect will be randomly pitched.
+    public float highPitchRange = 1.05f;            //The highest a sound effect will be randomly pitched.
+    bool fade = false;
+    bool changingMusic = false;
+    public float volume  = 0.1f;
+
+   AudioSource audioSource;
+
+    void Awake()
+    {
+        //Check if there is already an instance of SoundManager
+        if (instance == null)
+            //if not, set it to this.
+            instance = this;
+        //If instance already exists:
+        else if (instance != this)
+            //Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
+            Destroy(gameObject);
+
+        //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+        DontDestroyOnLoad(gameObject);
+        //PlaySingle(musicSource.GetComponent<AudioClip>());
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(musicClip1, 0.7f);
+        Debug.Log("sound manager OK");
+    }
+
+    void Update()
+    {
+        volume = audioSource.volume;
+        if (Input.GetKeyDown("space") || fade)
+        {
+            Debug.Log("space pushed");
+            FadeOut(musicClip2);
+        } else if (changingMusic)
+        {
+            FadeIn();
+        }
+    }
+
+    public void FadeIn()
+    {
+        Debug.Log("increasing volume");
+        audioSource.volume += 0.2f * Time.deltaTime;
+        if (audioSource.volume >= 0.7f)
+        {
+            changingMusic = false;
+        }
+    }
+
+    public void FadeOut(AudioClip audioClip)
+    {
+        Debug.Log("fading out");
+        fade = true;
+
+        if (audioSource.volume > 0.1f)// && !changingMusic)
+        {
+            //fade out
+            audioSource.volume -= 0.2f * Time.deltaTime;
+        }
+        else 
+        {
+            audioSource.Stop();
+            audioSource.PlayOneShot(audioClip, 0.1f);
+            fade = false;
+            changingMusic = true;
+        }
+            
+    }
+
+
+    //Used to play single sound clips.
+    public void PlaySingle(AudioClip clip)
+    {
+        Debug.Log("playing music!");
+        //Set the clip of our efxSource audio source to the clip passed in as a parameter.
+        /*efxSource.clip = clip;
+
+        //Play the clip.
+        efxSource.Play();
+        efxSource.volume = .7f;*/
+    }
+
+
+    //RandomizeSfx chooses randomly between various audio clips and slightly changes their pitch.
+    public void RandomizeSfx(params AudioClip[] clips)
+    {
+        /*//Generate a random number between 0 and the length of our array of clips passed in.
+        int randomIndex = Random.Range(0, clips.Length);
+
+        //Choose a random pitch to play back our clip at between our high and low pitch ranges.
+        float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+
+        //Set the pitch of the audio source to the randomly chosen pitch.
+        efxSource.pitch = randomPitch;
+
+        //Set the clip to the clip at our randomly chosen index.
+        efxSource.clip = clips[randomIndex];
+
+        //Play the clip.
+        efxSource.Play();*/
+    }
 }
